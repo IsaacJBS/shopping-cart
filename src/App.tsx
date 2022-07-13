561c0abe997d354b349ce65381cbe01bd5a03a55
Isaac <isaac.jordaobs@gmail.com>
2022-07-13 14:44:27 -0300

    feat: add some functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,44 @@
 import { useState } from "react";
 import { useQuery } from "react-query";
 
-import Drawer from "@material-ui/core";
-import LinearProgress from "@material-ui/core";
-import Grid from "@material-ui/core";
+import { Drawer } from "@material-ui/core";
+import { LinearProgress } from "@material-ui/core";
+import { Grid } from "@material-ui/core";
 import { AddShoppingCart } from "@material-ui/icons";
-import Badge from "@material-ui/core";
+import { Badge } from "@material-ui/core";
 
 import { Wrapper } from "./App.styles";
 
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+};
+
+const getProducts = async (): Promise<CartItemType[]> => {
+  return await (await fetch("https://fakestoreapi.com/products")).json();
+};
+
 const App = () => {
+  const { data, isLoading, error } = useQuery<CartItemType[]>(
+    "products",
+    getProducts,
+  );
+
+  const getTotalItems = () => null;
+
+  const handleAddToCart = () => null;
+
+  const handleFromCart = () => null;
+
+  if (isLoading) return <LinearProgress />;
+  if (error) return <div>Something went wrong</div>;
+
+  console.log(data);
   return (
     <div className="App">
       <header className="App-header">
