428e561987e093ef4385308ef4c10309cfc4deab
Isaac <isaac.jordaobs@gmail.com>
2022-07-13 15:12:04 -0300

    feat: styled item

--- src/Item/Item.styles.ts
+++ src/Item/Item.styles.ts
@@ -1,3 +1,28 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  border: 1px solid lightblue;
+  border-radius: 20px;
+
+  button {
+    border-radius: 0 0 20px 20px;
+  }
+
+  image {
+    max-height: 250px;
+    object-fit: cover;
+    border-radius: 20px 20px 0 0;
+  }
+
+  div {
+    font-family: Arial, Helvetica, sans-serif;
+    padding: 1rem;
+    height: 100%;
+  }
+`;
