4975baaf5e177cb0306885e5eee1ef7fd86d64ff
Isaac <isaac.jordaobs@gmail.com>
2022-07-13 14:33:09 -0300

    feat: style file

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,14 @@
+import { useState } from "react";
+import { useQuery } from "react-query";
+
+import Drawer from "@material-ui/core";
+import LinearProgress from "@material-ui/core";
+import Grid from "@material-ui/core";
+import { AddShoppingCart } from "@material-ui/icons";
+import Badge from "@material-ui/core";
+
+import { Wrapper } from "./App.styles";
+
 const App = () => {
   return (
     <div className="App">
